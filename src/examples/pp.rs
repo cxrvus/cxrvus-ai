42d1bbbd00d50fcce07d22657a075651cdaeab7f
Benito Rabe <benito.m.rabe@gmail.com>
2024-01-09 04:44:06 +0100

    PP progress

--- src/examples/pp.rs
+++ src/examples/pp.rs
@@ -1,3 +1,4 @@
+use rand::{Rng, thread_rng};
 use crate::bt::*;
 
 #[derive(Debug)]
@@ -5,15 +6,31 @@ struct BlackBoard {
 	thirst: u8,
 	bladder: u8,
 	drinking: bool,
-	pissing: bool
+	pissing: bool,
+	occupied: bool
 }
 
 impl BlackBoard {
 	fn default () -> Self {
-		Self { thirst: 100, bladder: 0, drinking: false, pissing: false }
+		Self { thirst: 100, bladder: 0, drinking: false, pissing: false, occupied: false }
 	}
 }
 
+
+fn random() -> u8 { thread_rng().gen_range(0..16) }
+fn coin() -> bool { thread_rng().gen_bool(0.5) }
+
 pub fn main () {
-	let tree = tree!(BlackBoard, Sequence(vec![]));
+	type T = BlackBoard;
+
+	let need2pee: LeafFunc<T> = |data| {
+		if data.bladder > 100 { Pass } else { Fail }
+	};
+
+	let pee: LeafFunc<T> = |data| {
+		let decr = random();
+		// TODO: fix mutability
+		if data.bladder >= decr { data.bladder -= decr; }
+		Wait
+	};
 }
