31c4f920924a069ee9d0956c96eabe52539b187c
Benito Rabe <benito.m.rabe@gmail.com>
2024-01-09 03:22:49 +0100

    major refac (&ditched imperative method)

--- src/bt.rs
+++ src/bt.rs
@@ -1,110 +1,94 @@
-use std::ops::Deref;
-
 use rand::{Rng, thread_rng};
+
+pub use Node::*;
+pub use State::*;
 
 
 #[derive(Debug, PartialEq)]
-enum State { Fail, Pass, Wait, Error(String) }
+enum State { Fail, Pass, Wait, Error(Option<String>) }
 
 #[derive(Debug)]
-enum Node {
-	Sequence (Vec<Node>),
-	Fallback (Vec<Node>),
-	Passer (Vec<Node>),
-	Random (Vec<Node>),
-
-	Inverter (Box<Node>),
-
-	Flow (Vec<Node>, FlowFunc),
-	Leaf (LeafFunc),
+enum Node<T> {
+	Leaf 		(LeafFunc<T>),
+	Flow 		(Vec<Node<T>>, FlowFunc<T>),
+	Sequence 	(Vec<Node<T>>),
+	Fallback 	(Vec<Node<T>>),
+	Random 		(Vec<Node<T>>),
+	Root 		(Box<Node<T>>),
+	Inverter 	(Box<Node<T>>),
+	Passer 		(Box<Node<T>>),
 }
 
 
-
-#[derive(Debug)]
-struct Tree<T> { root: Node, data: T }
-
-
-type FlowFunc = fn(&Vec<Node>) -> State;
-type LeafFunc = fn() -> State;
-
-use State::*;
+type FlowFunc<T> = fn(&Vec<Node<T>>, data: &T) -> State;
+type DecFunc<T> = fn(&Node<T>, data: &T) -> State;
+type LeafFunc<T> = fn(data: &T) -> State;
 
 
-impl<T> Tree<T> {
-	pub fn tick(&self) -> State {
-		let mut data: T = self.data;
-		let mut stack: Vec<(Node, usize)> = vec![];
-		let mut state: State;
+impl<T> Node<T> {
+	pub fn tick(&self, data: &T) -> State {
 
-
-		let push = |&node| { stack.push((node, 0)) };
-		let pop = || { stack.pop().expect("prevented by while") };
-		let peek = || { stack.last().expect("prevented by while") };
-		let cont = || {
-			let (x,i) = pop();
-			stack.push((x,i+1))
-		};
-
-
-		let sequence: FlowFunc = |nodes| {
-			continue_on(Pass, nodes)
-		};
-
-		let fallback: FlowFunc = |nodes| {
-			continue_on(Fail, nodes)
-		};
-
-		// let continue_on = |nodes: &Vec<Node>, cont_state: State| {
-		// 	for node in nodes {
-		// 		let child_state = node.tick();
-		// 		if child_state == cont_state { continue; }
-		// 		else { return child_state;}
-		// 	}
-		// 	return cont_state
-		// };
-
-
-		let passer: FlowFunc = |nodes| {
+		let sequence: FlowFunc<T> = |nodes, data| {
 			for node in nodes {
-				if let Error(error) = node.tick() { return Error(error); }
-			};
-			return Pass;
+				match node.tick(data) {
+					Pass => continue,
+					other => return other
+				}
+			}
+			Pass
 		};
 
-		let inverter = |node: &Node| {
-			match node.tick() {
+		let fallback: FlowFunc<T> = |nodes, data| {
+			for node in nodes {
+				match node.tick(data) {
+					Fail => continue,
+					other => return other
+				}
+			}
+			Fail
+		};
+
+		let random: FlowFunc<T> = |nodes, data| {
+			let mut rng = thread_rng();
+			let index = rng.gen_range(0..nodes.len());
+			nodes[index].tick(data)
+		};
+
+
+		let root: DecFunc<T> = |node, data| {
+			loop {
+				match node.tick(data) {
+					error @ Error(_) => return error,
+					_ => {}
+				}
+			}
+		};
+
+		let inverter = |node: &Node<T>, data: &T| -> State {
+			match node.tick(&data) {
 				Pass => Fail,
 				Fail => Pass,
 				other => other
 			}	
 		};
 
-
-		let random: FlowFunc = |nodes| {
-			let mut rng = thread_rng();
-			let index = rng.gen_range(0..nodes.len());
-			nodes[index].tick()
+		let passer: DecFunc<T> = |node, data| {
+			match node.tick(data) {
+				error @ Error(_) => error,
+				_ => Pass
+			}
 		};
 
-		while let Some((node, i)) = stack.last() {
-			state = match node {
-				Node::Sequence(nodes) => sequence(&nodes),
-				Node::Fallback(nodes) => fallback(&nodes),
-				Node::Passer(nodes) => passer(&nodes),
-				Node::Random(nodes) => random(&nodes),
-				Node::Inverter(node) => inverter(node.deref()),
-				_ => Error("unhandled node type".into())
-			}
+
+		match self {
+			Node::Leaf(func) => func(data),
+			Node::Flow(nodes, func) => func(nodes, data),
+			Node::Sequence(nodes) => sequence(nodes, data),
+			Node::Fallback(nodes) => fallback(nodes, data),
+			Node::Random(nodes) => random(nodes, data),
+			Node::Root(nodes) => root(nodes, data),
+			Node::Inverter(node) => inverter(node, data),
+			Node::Passer(node) => passer(node, data),
 		}
-
-		Error("".into())
 	}
-}
-
-
-
-struct BB { thirst_level: u8, pee_level: u8 }
-
-fn main () {
 }
